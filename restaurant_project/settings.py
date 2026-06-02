23a97534975d5e27bb6c44696d38c4eecd976318
Md. Nur Jamal Miah <nurjamalbabu949@gmail.com>
2026-06-02 16:55:38 +0600

    Enable DEBUG mode for troubleshooting

--- restaurant_project/settings.py
+++ restaurant_project/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-pj!^q=n3qcjkp+==%r5k!rr=%f(s=5)tmw@k-j+xery0(0b2+u')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
