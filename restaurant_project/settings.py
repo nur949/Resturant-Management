770f69776799f8f6d2bdb538d4b9cbe71ca9d702
Md. Nur Jamal Miah <nurjamalbabu949@gmail.com>
2026-06-02 15:29:21 +0600

    Fix 400 Bad Request by adding Render hostname to ALLOWED_HOSTS and setting CSRF_TRUSTED_ORIGINS

--- restaurant_project/settings.py
+++ restaurant_project/settings.py
@@ -30,6 +30,12 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = [h.strip() for h in config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',') if h.strip()]
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+CSRF_TRUSTED_ORIGINS = [f'https://{h}' for h in ALLOWED_HOSTS if h not in ['localhost', '127.0.0.1']]
+
 
 # Application definition
 
