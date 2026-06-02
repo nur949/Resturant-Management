82515df6596d41861280e869552c04908fe198aa
Md. Nur Jamal Miah <nurjamalbabu949@gmail.com>
2026-06-02 16:54:56 +0600

    Disable SECURE_SSL_REDIRECT for troubleshooting 500 error

--- restaurant_project/settings.py
+++ restaurant_project/settings.py
@@ -73,7 +73,7 @@ cloudinary.config(
 )
 
 # Security Hardening
-SECURE_SSL_REDIRECT = config('SECURE_SSL_REDIRECT', default=False, cast=bool)
+SECURE_SSL_REDIRECT = False
 SESSION_COOKIE_SECURE = config('SESSION_COOKIE_SECURE', default=False, cast=bool)
 CSRF_COOKIE_SECURE = config('CSRF_COOKIE_SECURE', default=False, cast=bool)
 SECURE_HSTS_SECONDS = config('SECURE_HSTS_SECONDS', default=0, cast=int)
